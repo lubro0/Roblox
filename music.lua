99f37c21b1d114673c755f7b5d35696cd96503a8
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-12-08 17:18:40 +0100

    Update music.lua

--- music.lua
+++ music.lua
@@ -1,11 +1,8 @@
 local soundId = "rbxassetid://9043887091"
 
-game.Players.PlayerAdded:Connect(function(player)
-    local sound = Instance.new("Sound")
-    sound.SoundId = soundId
-    sound.Volume = 1
-    sound.Looped = false
-    sound.PlayOnRemove = true
-    sound.Parent = workspace
-    sound:Destroy()
-end)
+local sound = Instance.new("Sound")
+sound.SoundId = soundId
+sound.Volume = 1
+sound.Looped = false
+sound.Parent = script.Parent
+sound:Play()
