da2d073474856d81ad3459338c62a10d65a44c95
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-12-08 17:08:52 +0100

    Update shutdown.lua

--- shutdown.lua
+++ shutdown.lua
@@ -1,13 +1,18 @@
+local MessagingService = game:GetService("MessagingService")
 local shutdownCommand = "!shutdown"
 local shutdownUser = "OoOCatMiai"
 local shutdownReason = "Shutdown"
 
+MessagingService:SubscribeAsync("ShutdownSignal", function()
+    for _, player in pairs(game.Players:GetPlayers()) do
+        player:Kick(shutdownReason)
+    end
+end)
+
 game.Players.PlayerAdded:Connect(function(player)
     player.Chatted:Connect(function(message)
         if message == shutdownCommand and player.Name == shutdownUser then
-            for _, targetPlayer in pairs(game.Players:GetPlayers()) do
-                targetPlayer:Kick(shutdownReason)
-            end
+            MessagingService:PublishAsync("ShutdownSignal", {})
         end
     end)
 end)
